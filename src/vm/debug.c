1b9d39c64ab900446726fe500feece7fe11e9368
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-05-12 22:13:22 -0400

    dissassembler for constant values

--- src/vm/debug.c
+++ src/vm/debug.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#include "bits/stdint-uintn.h"
 #include "chunk.h"
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
   printf("== %s ==\n", name);
@@ -12,15 +12,26 @@ void disassembleChunk(Chunk *chunk, const char *name) {
   }
 }
 
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
+  uint8_t constant = chunk->code[offset + 1];
+  printf("%-16s %4d", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("/n");
+  return offset + 2;
+}
+
 static int simpleInstruction(const char *name, int offset) {
   printf("%s\n", name);
   return offset + 1;
 }
+
 int disassembleInstruction(Chunk *chunk, int offset) {
   printf("%04d ", offset);
 
   uint8_t instruction = chunk->code[offset];
   switch(instruction) {
+  case OP_CONSTANT:
+    return constantInstruction("OP_CONSTANT", chunk, offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   default:
