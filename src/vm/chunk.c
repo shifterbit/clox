c4daa0f7d89e9d1af453a4193b7a69b0d793a106
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-05-13 14:35:51 -0400

    reduce chunk size with run-length encoding for lines

--- src/vm/chunk.c
+++ src/vm/chunk.c
@@ -9,6 +9,8 @@ void initChunk(Chunk *chunk) {
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  chunk->lineCount = 0;
+  chunk->lineCapacity = 0;
   chunk->lines = NULL;
   initValueArray(&chunk->constants);
 }
@@ -18,11 +20,27 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
   chunk->code[chunk->count] = byte;
-  chunk->lines[chunk->count] = line;
   chunk->count++;
+
+  // Check if we're still on the same line
+  if (chunk->lineCount > 0 && chunk->lines[chunk->lineCount -1].line == line) {
+    return;
+  }
+
+  // Append a new LineStart
+  if(chunk->lineCapacity < chunk->lineCount + 1) {
+    int oldCapacity = chunk->lineCapacity;
+    chunk->lineCapacity = GROW_CAPACITY(oldCapacity);
+    chunk->lines = GROW_ARRAY(LineStart, chunk->lines, oldCapacity, chunk->lineCapacity);
+
+  }
+
+  LineStart *lineStart = &chunk->lines[chunk->lineCount++];
+  lineStart->offset = chunk->count - 1;
+  lineStart->line = line;
+  
 }
 
 int addConstant(Chunk *chunk, Value value) {
@@ -32,7 +50,25 @@ int addConstant(Chunk *chunk, Value value) {
 
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-  FREE_ARRAY(int, chunk->lines, chunk->capacity);
+  FREE_ARRAY(LineStart, chunk->lines, chunk->lineCapacity);
   freeValueArray(&chunk->constants);
   initChunk(chunk);
+}
+
+
+int getLine(Chunk *chunk, int instruction) {
+  int start = 0;
+  int end = chunk->lineCount - 1;
+
+  for (;;) {
+    int mid = (start + end) / 2;
+    LineStart *line = &chunk->lines[mid];
+    if (instruction < line->offset) {
+      end = mid - 1;
+    } else if (mid == chunk->lineCount -1 || instruction < chunk->lines[mid + 1].offset) {
+      return line->line;
+    } else {
+      start = mid + 1;
+    }
+  }
 }
