e5683f5e65090ace05c4f5e09840677f1168c069
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-05-13 21:51:42 -0400

    write boilerplate for running instructions

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
   Chunk chunk;
+  initVM();
+  interpret(&chunk);
   initChunk(&chunk);
   int constant = addConstant(&chunk, 257);
   writeChunk(&chunk, OP_CONSTANT_LONG, 123);
@@ -12,6 +15,7 @@ int main(int argc, const char *argv[]) {
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
+  freeVM();
 
   return 0;
 }
