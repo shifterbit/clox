eaf7827d1f9006ebefb80c2faa4c46841d5121d8
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-06-04 03:30:08 -0400

    add reserved identifiers to scanner

--- src/parser/scanner.c
+++ src/parser/scanner.c
@@ -12,6 +12,12 @@ Scanner initScanner(const char *source) {
   return scanner;
 }
 
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') ||
+         (c >= 'A' && c <= 'Z') ||
+          c == '_';
+}
+
 static bool isDigit(char c) {
   return c >= '0' && c <= '9';
 }
@@ -95,6 +101,59 @@ static void skipWhitespace(Scanner *scanner) {
   }
 }
 
+static TokenType checkKeyword(Scanner *scanner, int start, int length,
+  const char* rest, TokenType type) {
+if (scanner->current - scanner->start == start + length &&
+    memcmp(scanner->start + start, rest, length) == 0) {
+  return type;
+}
+
+return TOKEN_IDENTIFIER;
+} 
+
+static TokenType identifierType(Scanner *scanner) {
+  switch (scanner->start[0]) {
+  case 'a':
+    return checkKeyword(scanner, 1, 2, "nd", TOKEN_AND);
+  case 'c':
+    return checkKeyword(scanner, 1, 4, "lass", TOKEN_CLASS);
+  case 'e':
+    return checkKeyword(scanner, 1, 3, "lse", TOKEN_ELSE);
+  case 'i':
+    return checkKeyword(scanner, 1, 1, "f", TOKEN_IF);
+  case 'n':
+    return checkKeyword(scanner, 1, 2, "il", TOKEN_NIL);
+  case 'o':
+    return checkKeyword(scanner, 1, 1, "r", TOKEN_OR);
+  case 'p':
+    return checkKeyword(scanner, 1, 4, "rint", TOKEN_PRINT);
+  case 'r':
+    return checkKeyword(scanner, 1, 5, "eturn", TOKEN_RETURN);
+  case 's':
+    return checkKeyword(scanner, 1, 4, "uper", TOKEN_SUPER);
+  case 't':
+        if (scanner->current - scanner->start > 1) {
+          switch (scanner->start[1]) {
+          case 'h': return checkKeyword(scanner, 2, 2, "is", TOKEN_THIS);
+          case 'r': return checkKeyword(scanner, 2, 2, "ue", TOKEN_TRUE);
+          }
+        }
+        break;
+  case 'v':
+    return checkKeyword(scanner, 1, 2, "ar", TOKEN_VAR);
+  case 'w':
+    return checkKeyword(scanner, 1, 4, "hile", TOKEN_WHILE);
+   }
+  return TOKEN_IDENTIFIER;
+}
+
+static Token identifier(Scanner *scanner) {
+  while (isAlpha(peek(scanner)) || isDigit(peek(scanner))) {
+    advance(scanner);
+  }
+  return makeToken(scanner ,identifierType(scanner));
+}
+
 static Token number(Scanner *scanner ) {
   while (isDigit(peek(scanner))) {
     advance(scanner);
@@ -133,9 +192,15 @@ Token scanToken(Scanner *scanner) {
   }
 
   char c = advance(scanner);
+
   if (isDigit(c)) {
     return number(scanner);
-      }
+  }
+
+  if (isAlpha(c)) {
+    return identifier(scanner);
+  }
+
   switch (c) {
   case '(':
     return makeToken(scanner, TOKEN_LEFT_PAREN);
